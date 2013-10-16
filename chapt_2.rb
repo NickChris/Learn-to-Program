97717a8afb091a857d0d1068983919a9f8125a45
Nick Chris <nicholas.chris@ttiltd.com>
2013-10-16 13:59:16 -0700

    chapter 2 exercises

---
+++ chapt_2.rb
@@ -0,0 +1,20 @@
+puts 2 + 1
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+puts 5/5*45
+puts (2+3)*(3+2)
+
+# Exercises
+
+puts 365 * 24 						# days in a year * hours= 8760 hours in a year
+puts 10*365*24*60					# decade * year * day * hour = 5256000 minutes in a decade
+puts 32*365*24*60*60				# age * year * day * hour * minute = 1009152000 age in seconds
+puts (((1160000000/60)/60)/24)/365 	# seconds/minute/hour/day/year  = 36 years old
